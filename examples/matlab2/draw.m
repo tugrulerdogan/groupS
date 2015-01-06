022f16b32eb27e20ef3cf730736077764ff47682
tugrulerdogan <h.tugrul.erdogan@gmail.com>
2015-01-06 06:03:58 +0200

    * Draw script updated

--- examples/matlab2/draw.m
+++ examples/matlab2/draw.m
@@ -8,10 +8,10 @@ base = 'D:\vot7\rests'
 image_base = 'D:\mtest\sequences' 
 
 % The name of the set
-tname = 'torus'
+tname = 'iceskater'
 
 % The frame number of visualisation
-frame_no = 15
+% frame_no = 15
 
 % The test methods
 itrts = {[1,2]}
@@ -19,10 +19,11 @@ itrts = {[1,2]}
 
 % ===============================
 
-
+colorstring = 'brycykm';
+labels = {'adaptive', 'intensity', 'Hog'};
 
 % open the frame imge
-
+h = figure(1); cla;
 imshow(sprintf('%s\\%s\\%08d.jpg', image_base, tname, frame_no));
 
 % draw the ground-truth
@@ -36,19 +37,36 @@ plot([gt(1) gt(1)],[gt(2)+gt(4) gt(2)],'g','linewidth', 2.5)
 
 
 % draw the results
-colorstring = 'brycykm';
+
 i = 1;
 for iterations = itrts
-iterations = cell2mat(iterations);
+    iterations = cell2mat(iterations);
 
-load(sprintf('%s\\result_%s_%s.mat', base, tname, strtrim(sprintf('%d', iterations))));
+    load(sprintf('%s\\result_%s_%s.mat', base, tname, strtrim(sprintf('%d', iterations))));
 
-rst = results(frame_no,:);
+    rst = results(frame_no,:);
 
-plot([rst(1) rst(3)], [rst(2) rst(4)], colorstring(i), 'linewidth', 2.5)
-plot([rst(3) rst(5)], [rst(4) rst(6)], colorstring(i), 'linewidth', 2.5)
-plot([rst(5) rst(7)], [rst(6) rst(8)], colorstring(i), 'linewidth', 2.5)
-plot([rst(7) rst(1)], [rst(8) rst(2)], colorstring(i), 'linewidth', 2.5)
+    plot([rst(1) rst(3)], [rst(2) rst(4)], colorstring(i), 'linewidth', 2.5)
+    plot([rst(3) rst(5)], [rst(4) rst(6)], colorstring(i), 'linewidth', 2.5)
+    plot([rst(5) rst(7)], [rst(6) rst(8)], colorstring(i), 'linewidth', 2.5)
+    plot([rst(7) rst(1)], [rst(8) rst(2)], colorstring(i), 'linewidth', 2.5)
+    
+    for itr = iterations
+        
+        i = i+1;
+        load(sprintf('%s\\result_%s_%s.mat', base, tname, strtrim(sprintf('%d', itr))));
 
-i = i+1;
+        rst = results(frame_no,:);
+
+        plot([rst(1) rst(3)], [rst(2) rst(4)], colorstring(i), 'linewidth', 2.5)
+        plot([rst(3) rst(5)], [rst(4) rst(6)], colorstring(i), 'linewidth', 2.5)
+        plot([rst(5) rst(7)], [rst(6) rst(8)], colorstring(i), 'linewidth', 2.5)
+        plot([rst(7) rst(1)], [rst(8) rst(2)], colorstring(i), 'linewidth', 2.5)
+        xlabel(sprintf('%s', labels{i}),'FontSize',12, 'Color', colorstring(i)); 
+        
+    end
+
+   
 end
+
+saveas(h,sprintf('%s\\jpgs\\result_%s_results_%s_%d.jpg', base, tname, strtrim(sprintf('%d', iterations)), frame_no));
